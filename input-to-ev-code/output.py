824257317a46d4e151d74c66a35972127228c9d3
chirbard <73120520+chirbard@users.noreply.github.com>
2021-08-11 15:48:43 +0300

    Update output.py

--- input-to-ev-code/output.py
+++ input-to-ev-code/output.py
@@ -8,14 +8,14 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 ev3 = EV3Brick()
-left_motor = Motor(Port.C)
-right_motor = Motor(Port.D)
+left_motor = Motor(Port.D)
+right_motor = Motor(Port.C)
 wheel_diameter = 56
 axle_track = 105
 robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
 
 robot.straight(500)
-robot.turn(-90)
+robot.turn(90)
 robot.straight(200)
 # grip
 robot.turn(180)
@@ -24,32 +24,35 @@ robot.turn(90)
 robot.straight(1400)
 # ungrip
 robot.straight(-100)
-robot.turn(-90)
+robot.turn(90)
 robot.straight(600)
-robot.turn(-90)
+robot.turn(90)
 robot.straight(500)
 # grip
 robot.turn(180)
 robot.straight(500)
-robot.turn(90)
+robot.turn(-90)
 robot.straight(500)
 # ungrip
 robot.straight(-100)
-robot.turn(90)
+robot.turn(-90)
 robot.straight(500)
 # grip
 robot.turn(180)
 robot.straight(300)
-robot.turn(90)
+robot.turn(-90)
 robot.straight(100)
 # ungrip
 robot.straight(-100)
-robot.turn(90)
+robot.turn(-90)
 robot.straight(1000)
-robot.turn(90)
+robot.turn(-90)
 robot.straight(500)
 # grip
 robot.turn(180)
 robot.straight(700)
 robot.turn(90)
 robot.straight(700)
+# ungrip
+robot.straight(-100)
+
